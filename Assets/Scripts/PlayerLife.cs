b7177a831afcbf27849b14cfa3e59e4751fada90
Fadel <fadel.foudi@gmail.com>
2023-12-13 02:29:59 -0500

    Feat: Life heart's player system (assets and update PlayerLife script

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -3,11 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerLife : MonoBehaviour
 {
     private Rigidbody2D player;
     private Animator anim;
+
+    [SerializeField] public float health;
+    [SerializeField] public float numberOfHearts;
+    private float trapDamages = 0.5f;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite halfHeart;
+    public Sprite emptyHeart;
 
     AudioManager audioManager;
 
@@ -18,15 +28,48 @@ public class PlayerLife : MonoBehaviour
 
     private void Start()
     {
-        
         anim = GetComponent<Animator>();
         player = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        UpdateHeartsUi();
+    }
+
+    private void UpdateHeartsUi()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            float heartValue = i + 1;
+            float displayValue = health - heartValue + 0.5f;
+
+            hearts[i].sprite = (displayValue > 0) ? fullHeart : (displayValue == 0) ? halfHeart : emptyHeart;
+            hearts[i].enabled = (heartValue <= numberOfHearts);
+
+            if (health > numberOfHearts)
+            {
+                health = numberOfHearts;
+            }
+        }
     }
 
     public void OnCollisionEnter2D(Collision2D collision) //TODO make protected
     {
         if (collision.gameObject.CompareTag("Trap"))
         {
+            ReduceHealth(trapDamages);
+        }
+    }
+
+    private void ReduceHealth(float amount)
+    {
+        health -= amount;
+        //TODO add damage sound
+        //TODO add invicibility time during hit
+        if (health <= 0)
+        {
+            health = 0;
             Death();
         }
     }
